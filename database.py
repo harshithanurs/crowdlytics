0ada5bd7679ef3326647d48078d7f840306c7fab
harshithanurs <harshithanurs011@gmail.com>
2026-05-17 19:58:23 +0530

    Configured Railway cloud database

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
+        host="hopper.proxy.rlwy.net",
         user="root",
-        password="",          # your password
-        database="crowdlytics",
+        password="hreREZRNOiharqMKjaLVZRRTBPqNYxkJ",
+        database="railway",
+        port=int("23564"),
         autocommit=True,
         connection_timeout=10
     )
